5b61277a02e62bef2e8c6bcc22fd062592c17280
Roj Tamang <tamangroj56@gmail.com>
2024-10-05 12:53:43 +0545

    Added parent child relationship in category

--- product/models.py
+++ product/models.py
@@ -4,7 +4,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=200, blank=True, null=True)
-
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True, related_name='children')
+    
     def __str__(self):
         return self.name
 
